6c3ff47dddcfba390b3589b76c91cb75f92ab832
seoyoomi <seoyoomi1102@gmail.com>
2026-03-18 15:20:42 +0900

    fix: build.gradlew dependencies 추가

--- user-service/src/main/java/com/inspire/user/dao/UserDao.java
+++ user-service/src/main/java/com/inspire/user/dao/UserDao.java
@@ -2,4 +2,6 @@ package com.inspire.user.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.inspire.user.domain.entity.UserEntity;
+
 public interface UserDao extends JpaRepository<UserEntity, Long> {}
